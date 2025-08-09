c56b7c22026d07790d6f3dfe5a193d0739cb1f7f
brian070069 <kkipngeno409@gmail.com>
2025-08-09 16:30:15 +0300

    created the model, service and repository packages

---
+++ employeemanager/src/main/java/com/aasn/repositories/EmployeeRepo.java
@@ -0,0 +1,12 @@
+package com.aasn.repositories;
+
+import com.aasn.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface EmployeeRepo extends JpaRepository<Employee, Long> {
+    void deleteEmployeeById(Long id);
+
+    Optional findEmployeeById(Long id);
+}
